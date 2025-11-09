be02a659738976f4fa21493896f85b0b67c18a91
Aleksei <aleksei.kotliarov@epita.fr>
2025-11-09 01:48:29 +0100

    upd

--- app/main.py
+++ app/main.py
@@ -1,36 +1,43 @@
-from fastapi import FastAPI
+import os
 from typing import List
+
 import mlflow
 import numpy as np
-import os
+from fastapi import FastAPI
 
 app = FastAPI()
-model_name = 'tracking-quickstart'
-model_version = 'latest'
+model_name = "tracking-quickstart"
+model_version = "latest"
 
-mlflow_uri = os.getenv('MLFLOW_TRACKING_URI')
+mlflow_uri = os.getenv("MLFLOW_TRACKING_URI")
 mlflow.set_tracking_uri(uri=f"{mlflow_uri}")
 
 app.model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_version}")
 
+
 def load_model(version: str | int):
     app.model = mlflow.pyfunc.load_model(f"models:/{model_name}/{version}")
 
+
 @app.get("/health")
 async def check_health():
-    return 'OK'
+    return "OK"
+
 
 # Sepal Length, Sepal Width, Petal Length and Petal Width.
 @app.post("/predict")
 async def create_prediction(inputs: List[float]):
     return app.model.predict(np.array(inputs).reshape((-1, 4))).tolist()
 
+
 from pydantic import BaseModel
+
 
 class VersionInput(BaseModel):
     version: str | int
-    
-@app.post('/update-model')
+
+
+@app.post("/update-model")
 async def update_model(inputs: VersionInput):
     try:
         load_model(inputs.version)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from tp-mlflow!")
+
+
+if __name__ == "__main__":
+    main()
