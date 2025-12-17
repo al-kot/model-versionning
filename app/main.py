a956e54c5b378a29c034594be994f3207d542bae
Aleksei <aleksei.kotliarov@epita.fr>
2025-12-17 10:40:25 +0100

    accept-next

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import os
+import random
 from typing import List
 
 import mlflow
 import numpy as np
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 model_name = "tracking-quickstart"
@@ -12,12 +14,13 @@ model_version = "latest"
 mlflow_uri = os.getenv("MLFLOW_TRACKING_URI")
 mlflow.set_tracking_uri(uri=f"{mlflow_uri}")
 
-app.model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_version}")
+# Load initial model
+initial_model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_version}")
 
+app.current_model = initial_model
+app.next_model = initial_model
 
-def load_model(version: str | int):
-    app.model = mlflow.pyfunc.load_model(f"models:/{model_name}/{version}")
-
+CANARY_PROBABILITY = 0.8
 
 @app.get("/health")
 async def check_health():
@@ -27,10 +30,12 @@ async def check_health():
 # Sepal Length, Sepal Width, Petal Length and Petal Width.
 @app.post("/predict")
 async def create_prediction(inputs: List[float]):
-    return app.model.predict(np.array(inputs).reshape((-1, 4))).tolist()
-
-
-from pydantic import BaseModel
+    if random.random() < CANARY_PROBABILITY:
+        model = app.current_model
+    else:
+        model = app.next_model
+        
+    return model.predict(np.array(inputs).reshape((-1, 4))).tolist()
 
 
 class VersionInput(BaseModel):
@@ -40,7 +45,12 @@ class VersionInput(BaseModel):
 @app.post("/update-model")
 async def update_model(inputs: VersionInput):
     try:
-        load_model(inputs.version)
+        app.next_model = mlflow.pyfunc.load_model(f"models:/{model_name}/{inputs.version}")
     except:
         return False
+    return True
+
+@app.post("/accept-next-model")
+async def accept_next_model():
+    app.current_model = app.next_model
     return True
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from tp-mlflow!")
-
-
-if __name__ == "__main__":
-    main()
